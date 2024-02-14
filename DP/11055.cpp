d994af2b74de6cce2a872dd10371bc81eccfc04b
HeonWoo Kim <98148896+WOONBE@users.noreply.github.com>
2024-02-14 10:46:38 +0900

    Create 11055.cpp

---
+++ DP/11055.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int n;
+int arr[1010];
+int d[1010];
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+		d[i] = arr[i];
+		
+	}
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j < i; j++) {
+			if (arr[j] < arr[i]) {
+				d[i] = max(d[i], d[j] + arr[i]); //아이디어는 맞았는데 출력할때 문제
+			}
+		}
+	}
+	cout << *max_element(d + 1, d+ n+ 1);
+	
+
+}
