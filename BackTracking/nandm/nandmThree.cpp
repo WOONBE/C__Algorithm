6ffc67456f5c1658e8ceef3ead4f97216909d337
HeonWoo Kim <98148896+WOONBE@users.noreply.github.com>
2024-03-26 10:32:11 +0900

    Update nandmThree.cpp

--- BackTracking/nandm/nandmThree.cpp
+++ BackTracking/nandm/nandmThree.cpp
@@ -12,7 +12,7 @@ void func(int k){ // 현재 k개까지 수를 택했음.
         for(int i = 0; i < m; i++)
             cout << arr[i] << ' '; // arr에 기록해둔 수를 출력
         cout << '\n';
-        return;
+        return; //return 안하면 터짐
     } //여기까진 동일
     for(int i = 1; i <= n; i++){
         arr[k] = i; // k번째 수를 i로 정함
