b417ba04920c4e1acc2eaeb84cc7516c05bb720f
HeonWoo Kim <98148896+WOONBE@users.noreply.github.com>
2024-04-01 11:36:26 +0900

    Update lab.py

--- swea/for_make_up_class/lab.py
+++ swea/for_make_up_class/lab.py
@@ -1,10 +1,9 @@
-#for 0402 study
 from collections import deque
 
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-def bfs():
+def count_virus():
     q = deque(virus)
     vis = [[False] * m for _ in range(n)]
     ret = 0
@@ -18,32 +17,32 @@ def bfs():
                 q.append((nx, ny))
     return ret
 
-def dfs(k, idx):
+def make_wall(k, idx):
     global ans
     if k == 3:
-        tmp = bfs()
+        tmp = count_virus()
         ans = max(ans, free_cells - 3 - tmp + len(virus))
         return
     for i in range(idx, len(frees)):
         x, y = frees[i]
         board[x][y] = 1
-        dfs(k + 1, i + 1)
+        make_wall(k + 1, i + 1)
         board[x][y] = 0
 
-if __name__ == "__main__":
-    n, m = map(int, input().split())
-    board = []
-    virus = []
-    free_cells = 0
-    ans = 0
-    for i in range(n):
-        row = list(map(int, input().split()))
-        board.append(row)
-        for j in range(m):
-            if row[j] == 0:
-                free_cells += 1
-            elif row[j] == 2:
-                virus.append((i, j))
-    frees = [(i, j) for i in range(n) for j in range(m) if board[i][j] == 0]
-    dfs(0, 0)
-    print(ans)
+
+n, m = map(int, input().split())
+board = []
+virus = []
+free_cells = 0
+ans = 0
+for i in range(n):
+    row = list(map(int, input().split()))
+    board.append(row)
+    for j in range(m):
+        if row[j] == 0:
+            free_cells += 1
+        elif row[j] == 2:
+            virus.append((i, j))
+frees = [(i, j) for i in range(n) for j in range(m) if board[i][j] == 0]
+make_wall(0, 0)
+print(ans)
