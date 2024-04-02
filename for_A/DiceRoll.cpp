99e8d37d526ccfc6db66be0daa170b407065e3e8
WOONBE <kepull2918@naver.com>
2024-04-02 19:53:30 +0900

    0401 주사위굴리기2

--- for_A/DiceRoll.cpp
+++ for_A/DiceRoll.cpp
@@ -1,8 +1,5 @@
-#include <vector>
 #include <iostream>
-#include <tuple>
 #include <algorithm>
-#include <utility>
 using namespace std;
 
 int n, m, x, y, k, dir;
@@ -13,19 +10,19 @@ int dice[6] = {0,0,0,0,0,0};
 
 
 bool OOB(int r, int c) {
-	return r <= 0 || r > n || c <= 0 || c > m;
+    return r < 0 || r >= n || c < 0 || c >= m;
 }
 
 void input() {
-	cin >> n >> m >> x >> y >> k;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j];
-		}
-	}
+    cin >> n >> m >> x >> y >> k;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
 }
 
-void move() {
+void move(int dir) {
     int tmp[6];
     copy(begin(dice), end(dice), begin(tmp));
 
@@ -58,34 +55,38 @@ void move() {
     y += dy[dir];
 }
 void setScore() {
+    int dice_top = dice[0];
     int dice_bottom = dice[5];
     if (OOB(x, y)) {
+        int cdir = (dir+2) %4;
+        move(cdir);
         return;
     }
     if (arr[x][y] == 0) {
         arr[x][y] = dice_bottom;
     }
-    else if (arr[x][y] != 0) {
-        dice_bottom = arr[x][y];
-        dice[5] = dice_bottom;
+    else {
+        dice[5] = arr[x][y];
+        arr[x][y] = 0; //이부분 실수
     }
+    cout << dice_top << '\n';
 }
 
 
 int main() {
-	input();
+    input();
 
-	while (k--) {
-		cin >> dir;
-		//dirnum 기준으로 돌리기
+    while (k--) {
+        cin >> dir;
+        //dirnum 기준으로 돌리기
         dir--;
-        move();
+        move(dir);
         setScore();
-        if (OOB(x, y)) {
+        /*if (OOB(x, y)) {
             continue;
         }
         else{
             cout << dice[0] << "\n";
-        }
-	}
+        }*/
+    }
 }
